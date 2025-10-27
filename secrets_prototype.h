ea1da31d777e22c1d6bcfcc22fb19bf5ed334512
bill-orange <billwebb12@gmail.com>
2025-10-27 06:54:23 -0700

    Add files via upload

---
+++ secrets_prototype.h
@@ -0,0 +1,14 @@
+// Rename this file secrets.h
+// Enter your Wifi and OpenAI API key below
+//
+const char *ssid_3 = "your network";
+const char *password_3 = "your password";
+
+#define GITHUBURL "your file source"
+const char *chatGPT_APIKey_txt = "your api key for prompt";
+const char *chatGPT_APIKey = "your api key for TTS must start with Bearer";
+const char *ssid_1 = "your network";
+const char *password_1 = "your password";
+const char *ssid_2 = "your network";
+const char *password_2 = "your password";
+
